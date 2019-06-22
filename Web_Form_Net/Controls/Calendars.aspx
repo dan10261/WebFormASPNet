e4aca92ddc5086011a2456f006ede5a0fba2c1af
Dan Du <DDU@sba.gov>
2019-06-22 13:21:42 -0400

    Commit calendar practice

---
+++ Web_Form_Net/Controls/Calendars.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Calendars.aspx.cs" Inherits="Web_Form_Net.Controls.Calendars" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="header" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="row form-group col-md-6">
+            <div class="col-md-6">
+                <asp:Label runat="server" AssociatedControlID="ddlYears" Text="Year" CssClass="control-label"></asp:Label>
+                <asp:DropDownList runat="server" ID="ddlYears" CssClass="form-control" AutoPostBack="true" OnSelectedIndexChanged="ddlYearsMonths_SelectedIndexChanged"></asp:DropDownList>
+            </div>
+            <div class="col-md-6">
+                <asp:Label runat="server" AssociatedControlID="ddlMonths" Text="Month" CssClass="control-label"></asp:Label>
+                <asp:DropDownList runat="server" ID="ddlMonths" CssClass="form-control" AutoPostBack="true" OnSelectedIndexChanged="ddlYearsMonths_SelectedIndexChanged"></asp:DropDownList>
+            </div>
+        </div>
+        <div class="row form-group col-md-12">
+            <div class="col-md-12">
+                <asp:Calendar runat="server" ID="CldrDate" ></asp:Calendar>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ Web_Form_Net/Controls/Calendars.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_Form_Net.Controls
+{
+    public partial class Calendars : System.Web.UI.Page
+    {
+        #region public methods
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadMonths();
+                LoadYears();
+            }
+        }
+        protected void ddlYearsMonths_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CldrDate.SelectedDate = new DateTime(Convert.ToInt32(ddlYears.SelectedValue),Convert.ToInt16(ddlMonths.SelectedValue),1);
+            CldrDate.VisibleDate = new DateTime(Convert.ToInt32(ddlYears.SelectedValue), Convert.ToInt16(ddlMonths.SelectedValue), 1);
+        }
+
+        #endregion public methods
+
+        #region  private methods 
+        #region Load Data
+        private void LoadMonths()
+        {
+            DataSet DSMonths = new DataSet();
+            DSMonths.ReadXml(Server.MapPath("~/Resources/XML/Months.xml"));
+            ddlMonths.DataSource = DSMonths;
+
+            ddlMonths.DataTextField = "Number";
+            ddlMonths.DataValueField = "Value";
+            ddlMonths.DataBind();
+        }
+        private void LoadYears()
+        {
+            DataSet DSYears = new DataSet();
+            DSYears.ReadXml(Server.MapPath("~/Resources/XML/Years.xml"));
+            ddlYears.DataSource = DSYears;
+            ddlYears.DataTextField = "Number";
+            ddlYears.DataValueField = "Value";
+            ddlYears.DataBind();
+        }
+        #endregion Load Date
+
+        #endregion private methods
+
+      
+    }
+}
